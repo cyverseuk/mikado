d052e53c3286ea418f1fd581223e984246c5fed3
Alice Minotto <i.m.e@hotmail.it>
2016-10-25 17:22:09 +0100

    Update util_wrapper.sh
    fixed for agave case insensitivity

--- wrapper/util_wrapper.sh
+++ wrapper/util_wrapper.sh
@@ -2,101 +2,101 @@
 
 ARGS="${pos_arg} ${region} ${chromosome} ${as} ${start} ${end} ${gtf} ${gf} ${out_format} ${id_file} ${gff} ${ann} ${xml}"
 
-UTIL="${pos_arg}"
-CHR="${chromosome}"
-REG="${region}"
-GTF="${gtf}"
-GF="${gf}"
-OUTFOR="${out_format}"
-ID_FILE="${id_file}"
-GFF="${gff}"
-ANN="${ann}"
-XML="${xml}"
+UTILU="${pos_arg}"
+CHRU="${chromosome}"
+REGU="${region}"
+GTFU="${gtf}"
+GFU="${gf}"
+OUTFORU="${out_format}"
+ID_FILEU="${id_file}"
+GFFU="${gff}"
+ANNU="${ann}"
+XMLU="${xml}"
 
 #########awk_gtf validity check and command line
-if [ "${UTIL}" == "awk_gtf " ]
+if [ "${UTILU}" == "awk_gtf " ]
   then
-    if [ -z "${GTF}" ]
+    if [ -z "${GTFU}" ]
       then
         echo "gtf file is required for awk_gtf utility"
         exit 1;
     fi
-    if [ -z "${CHR}" ] && [ -z "${REG}" ]
+    if [ -z "${CHRU}" ] && [ -z "${REGU}" ]
       then
         echo "one of the two options --chrom or --region is required for awk_gtf utility"
         exit 1;
     fi
-    if [  -n "${CHR}" ] && [ -n "${REG}" ]
+    if [  -n "${CHRU}" ] && [ -n "${REGU}" ]
       then
           echo "--chrom and --region are mutually exclusive options"
           exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${REG} ${CHR} ${as} ${start} ${end} ${GTF} awk_gtf.out"
+    CMDLINEARG="mikado util ""${UTILU} ${REGU} ${CHRU} ${as} ${start} ${end} ${GTFU} awk_gtf.out"
 fi
 
 ##########convert validity check and command line
-if [ "${UTIL}" == "convert " ]
+if [ "${UTILU}" == "convert " ]
   then
-    if [ -z "${GF}" ]
+    if [ -z "${GFU}" ]
       then
         echo "input file is required for convert utility"
         exit 1;
     fi
-    if [ -z "${OUTFOR}" ]
+    if [ -z "${OUTFORU}" ]
       then
         echo "choose output format for the conversion"
         exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${OUTFOR} ${GF} convert.out"
+    CMDLINEARG="mikado util ""${UTILU} ${OUTFORU} ${GFU} convert.out"
 fi
 
 ###########grep validity check and command line
-if [ "${UTIL}" == "grep " ]
+if [ "${UTILU}" == "grep " ]
   then
-    if [ -z "${ID_FILE}" ] || [ -z "${GFF}" ]
+    if [ -z "${ID_FILEU}" ] || [ -z "${GFFU}" ]
       then
         echo "id file and gff are required for grep utility"
         exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${grep_v} ${genes} ${ID_FILE} ${GFF} grep.out"
+    CMDLINEARG="mikado util ""${UTILU} ${grep_v} ${genes} ${ID_FILEU} ${GFFU} grep.out"
 fi
 
 ##########stats validity check and command line
-if [ "${UTIL}" == "stats " ]
+if [ "${UTILU}" == "stats " ]
   then
-    if [ -z "${GFF}" ]
+    if [ -z "${GFFU}" ]
       then
         echo "gff file is required for stats utility"
         exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${only_coding} ${GFF} stats.out"
+    CMDLINEARG="mikado util ""${UTILU} ${only_coding} ${GFFU} stats.out"
 fi
 
 #######trim validity check and command line
-if [ "${UTIL}" == "trim " ]
+if [ "${UTILU}" == "trim " ]
   then
-    if [ -z "${ANN}" ]
+    if [ -z "${ANNU}" ]
       then
         echo "annotation file is required for trim utility"
         exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${max_len} ${as_gtf} ${ANN} trim.out"
+    CMDLINEARG="mikado util ""${UTILU} ${max_len} ${as_gtf} ${ANNU} trim.out"
 fi
 
 #########merge_blast validity check and command line
-if [ "${UTIL}" == "merge_blast " ]
+if [ "${UTILU}" == "merge_blast " ]
   then
-    if [ -z "${XML}" ]
+    if [ -z "${XMLU}" ]
       then
         echo "xml files are required for merge_blast utility"
         exit 1;
     fi
-    CMDLINEARG="mikado util ""${UTIL} ${verbose} --log merge_blast.log --out merge_blast.out ${XML}"
+    CMDLINEARG="mikado util ""${UTILU} ${verbose} --log merge_blast.log --out merge_blast.out ${XMLU}"
 fi
 
 echo arguments are "${CMDLINEARG}"
-INPUTS="${gtf}, ${gf}, ${id_file}, ${gff}, ${xml}, ${ann}"
-echo input files are "${INPUTS}"
+INPUTSU="${gtf}, ${gf}, ${id_file}, ${gff}, ${xml}, ${ann}"
+echo input files are "${INPUTSU}"
 
 chmod +x launch.sh
 
@@ -104,7 +104,7 @@ echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/mikado:v1.0 >> lib/condorSubmitEdit.htc ######
 echo executable               =  ./launch.sh >> lib/condorSubmitEdit.htc #####
 echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
-echo transfer_input_files = ${INPUTS}, launch.sh >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
 echo transfer_output_files = output >> lib/condorSubmitEdit.htc
 cat /mnt/data/rosysnake/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
