988dad1ac735a252e9ca9f19558a51dc268d7493
Alice Minotto <i.m.e@hotmail.it>
2016-10-25 17:23:01 +0100

    Update compare_wrapper.sh
    fixed for agave case insensitivity

--- wrapper/compare_wrapper.sh
+++ wrapper/compare_wrapper.sh
@@ -3,10 +3,10 @@
 ARGS=" ${reference} ${targets} ${prediction} ${distance} ${protein_coding} ${lenient} ${exclude_utr} ${verbose} -l compare.log"
 #echo $ARGS
 
-TARGETS="${targets}"
-REF="${reference}"
+TARGETSU="${targets}"
+REFU="${reference}"
 
-if [  "${TARGETS}" == "--prediction " ]
+if [  "${TARGETSU}" == "--prediction " ]
   then
     if [ -z "${prediction}" ]
       then
@@ -15,11 +15,11 @@ if [  "${TARGETS}" == "--prediction " ]
     fi
 fi
 
-CMDLINEARG="mikado compare ""${REF}"" --index; mikado compare ""${ARGS}"
+CMDLINEARG="mikado compare ""${REFU}"" --index; mikado compare ""${ARGS}"
 echo arguments are "${CMDLINEARG}"
-REF=(${REF})
-INPUTS="${REF[@]:1}, ${prediction}"
-echo inputs are "${INPUTS}"
+REFU=(${REFU})
+INPUTSU="${REFU[@]:1}, ${prediction}"
+echo inputs are "${INPUTSU}"
 
 chmod +x launch.sh
 
@@ -27,8 +27,8 @@ echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/mikado:v1.0 >> lib/condorSubmitEdit.htc ######
 echo executable               =  ./launch.sh >> lib/condorSubmitEdit.htc #####
 echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
-echo transfer_input_files = ${INPUTS}, launch.sh >> lib/condorSubmitEdit.htc
-echo transfer_output_files = output/compare.log, output/mikado_compare.refmap, output/mikado_compare.stats, output/mikado_compare.tmap, output/${REF[@]:1}.midx >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
+echo transfer_output_files = output/compare.log, output/mikado_compare.refmap, output/mikado_compare.stats, output/mikado_compare.tmap, output/${REFU[@]:1}.midx >> lib/condorSubmitEdit.htc
 
 cat /mnt/data/rosysnake/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
